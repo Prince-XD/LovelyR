a69d36ac41174a1ea839ce75d7c39aaf1b0f0d15
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-01-24 23:39:04 +0530

    Update logomaker.py

--- Zaid/modules/logomaker.py
+++ Zaid/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Zaid Robot](https://t.me/Zaid2_Robot)",
+            caption=Logo by [Lovely](https://t.me/LOVELYR_OBOT)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Zaid](https://t.me/Zaid2_Robot)",
+            caption="Logo by [Lovely](https://t.me/LOVELYR_OBOT)",
             force_document=False,
         )
         os.remove(flnme)
