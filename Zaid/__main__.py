a41b1dd297e184afd5e3d4de86df239b3429deea
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-01-27 00:55:55 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -305,6 +305,33 @@ def error_handler(update, context):
 
 
 # for test purposes
+def error_callback(update: Update, context: CallbackContext):
+    error = context.error
+    try:
+        raise error
+    except Unauthorized:
+        print("no nono1")
+        print(error)
+        # remove update.message.chat_id from conversation list
+    except BadRequest:
+        print("no nono2")
+        print("BadRequest caught")
+        print(error)
+
+        # handle malformed requests - read more below!
+    except TimedOut:
+        print("no nono3")
+        # handle slow connection problems
+    except NetworkError:
+        print("no nono4")
+        # handle other connection problems
+    except ChatMigrated as err:
+        print("no nono5")
+        print(err)
+        # the chat_id of a group has changed, use e.new_chat_id instead
+    except TelegramError:
+        print(error)
+        # handle all other telegram related errors
 
 
 def help_button(update, context):
@@ -933,6 +960,7 @@ def main():
     dispatcher.add_handler(migrate_handler)
     dispatcher.add_handler(donate_handler)
 
+    dispatcher.add_error_handler(error_callback)
 
     if WEBHOOK:
         LOGGER.info("Using webhooks.")
