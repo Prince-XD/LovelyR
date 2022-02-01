5cdc332f6715df870aa53977c72643f17496ab7b
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-01 15:06:42 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -723,7 +723,7 @@ Again thanks for using me
             ),
         )
     elif query.data == "lovelyx_vida":
-        query.edit_video(
+        query.message.reply_video(
             LOVELYX_VIDAA,
             parse_mode=ParseMode.MARKDOWN,         
             reply_markup=InlineKeyboardMarkup(
