1d440bf22dc001e743ef090aad1162d0bab42673
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-01 07:32:24 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -418,7 +418,7 @@ def emiko_about_callback(update, context):
             reply_markup=InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="Basic Commands", callback_data="emiko_admin"),
+                    InlineKeyboardButton(text="Basic Commands", callback_data="lovelyx_admin"),
                     InlineKeyboardButton(text="Advanced Commands", callback_data="emiko_support"),
                  ],      
                  [
@@ -725,6 +725,42 @@ Again thanks for using me
         query.message.reply_video(
             LOVELYX_VIDAA,
             parse_mode=ParseMode.MARKDOWN,           
+        )
+
+elif query.data == "lovelyx_admin":
+        query.message.edit_text(
+            text="""Here is the help for the Admins module:
+
+User Commands:
+❂ /admins: list of admins in the chat
+❂ /pinned: to get the current pinned message.
+
+The Following Commands are Admins only: 
+❂ /pin: silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+❂ /unpin: unpins the currently pinned message
+❂ /invitelink: gets invitelink
+❂ /promote: promotes the user replied to
+❂ /fullpromote: promotes the user replied to with full rights
+❂ /demote: demotes the user replied to
+❂ /title <title here>: sets a custom title for an admin that the bot promoted
+❂ /admincache: force refresh the admins list
+❂ /del: deletes the message you replied to
+❂ /purge: deletes all messages between this and the replied to message.
+❂ /purge <integer X>: deletes the replied message, and X messages following it if replied to a message.
+❂ /setgtitle <text>: set group title
+❂ /setgpic: reply to an image to set as group photo
+❂ /setdesc: Set group description
+❂ /setsticker: Set group sticker
+
+Rules:
+❂ /rules: get the rules for this chat.
+❂ /setrules <your rules here>: set the rules for this chat.
+❂ /clearrules: clear the rules for this chat.""",
+            parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(
+              [[InlineKeyboardButton(text="back", callback_data="lovelyx_")]              
+              ]
+            ),
         )
 
 def source_about_callback(update, context):
