d57b64c0e12052083d745658d158aaf640570870
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-02 14:58:58 +0530

    Update alive.py

--- Zaid/modules/alive.py
+++ Zaid/modules/alive.py
@@ -9,16 +9,16 @@ from Zaid.events import register
 from Zaid import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/be24bbabbe0ec30dff489.jpg"
+VIDEO = "https://telegra.ph/file/1e75382f8192606545f11.mp4"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Zaid Robot.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Lovely Robot.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [Zaid](https://t.me/Timesisnotwaiting)** \n\n"
+  TEXT += f"⚪ **My Master : [Tushar](https://t.me/Tushar204)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Zaid2_Robot?start=help"), Button.url("Support", "https://t.me/Superior_Support")]]
+  BUTTON = [[Button.url("Help", "https://t.me/LOVELYR_OBOT?start=help"), Button.url("Support", "https://t.me/LOVELYAPPEAL")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
