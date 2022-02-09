4766f5975b77554ff323af65c59d5bb5f87c556c
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-09 18:26:19 +0530

    Create tagalerts.py

---
+++ Zaid/modules/tagalerts.py
@@ -0,0 +1,11 @@
+__help__ = """
+If anyone tagged/mentioned in a group where Lovely is present
+Lovely will notify it to you in private message after enabling tag alerts
+
+How to use this feature ?
+❂ `/tagalert on` : Turn tag alerts on
+❂ `/tagalert off` : Turn tag alerts off
+
+__mod_name__ = "Tag alert"
+
+__lovely_advance__ = __help__
