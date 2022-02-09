7092ae07f3fb4f01051a36b1a83d268d6d514652
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-09 14:36:13 +0530

    Update userinfo.py

--- Zaid/modules/userinfo.py
+++ Zaid/modules/userinfo.py
@@ -562,14 +562,6 @@ Examples:
  
 *json Detailed info:*
 ❂ /json*:* Get Detailed info about any message.
- 
-*AFk:*
-When marked as AFK, any mentions will be replied to with a message stating that you're not available!
-❂ /afk <reason>*:* Mark yourself as AFK.
-  - brb <reason>: Same as the afk command, but not a command. 
-  
-*What is that health thingy?*
- Come and see [HP System explained](https://t.me/KennedyProject/44)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio, run_async=True)
@@ -592,7 +584,8 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Info & AFK"
+__mod_name__ = "Info"
+__lovely_basic__ = __help__
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
