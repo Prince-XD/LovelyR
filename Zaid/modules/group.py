b4fedc7aa30b0bdc4ad6c919c4b819d09024e541
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-09 14:14:49 +0530

    Create group.py

---
+++ Zaid/modules/group.py
@@ -0,0 +1,11 @@
+__help__ = """
+*Admins only*:
+❂ /setgtitle <text>: set group title
+❂ /setgpic: reply to an image to set as group photo
+❂ /setdesc: Set group description
+❂ /setsticker: Set group sticker
+"""
+
+__mod_name__ = "Group Tool"
+
+__lovely_basic__ = __help__
