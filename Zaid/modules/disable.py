8ae29d6be3ef0ea179867979475a02ba636f1c94
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-09 14:03:54 +0530

    Update disable.py

--- Zaid/modules/disable.py
+++ Zaid/modules/disable.py
@@ -328,7 +328,6 @@ if is_module_loaded(FILENAME):
 ❂ /disablemodule <module name>*:* disable all commands in that module
 ❂ /listcmds*:* list all possible toggleable commands
 """
-   __lovely_basic__ = __help__
 
     DISABLE_HANDLER = CommandHandler("disable", disable, run_async=True)
     DISABLE_MODULE_HANDLER = CommandHandler(
