901ca8490a1a2f1ba87a85b2b14d50fdadab4179
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-09 14:20:47 +0530

    Create mutings.py

---
+++ Zaid/modules/mutings.py
@@ -0,0 +1,9 @@
+__help__ = """
+❂ /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
+❂ /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
+❂ /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user. 
+"""
+
+__lovely_basic__ = __help__
+
+__mod_name__ = "Mute"
