abfe7f6270dc359993eb7eb202953a5a2a78e53f
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-09 14:18:24 +0530

    Create disabling.py

---
+++ Zaid/modules/disabling.py
@@ -0,0 +1,15 @@
+__help__ = """
+❂ /cmds*:* check the current status of disabled commands
+
+*Admins only:*
+
+❂ /enable <cmd name>*:* enable that command
+❂ /disable <cmd name>*:* disable that command
+❂ /enablemodule <module name>*:* enable all commands in that module
+❂ /disablemodule <module name>*:* disable all commands in that module
+❂ /listcmds*:* list all possible toggleable commands
+"""
+
+__lovely_basic__ = __help__
+
+__mod_name__ = "Disable"
