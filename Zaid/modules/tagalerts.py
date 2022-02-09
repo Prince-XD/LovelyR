c0f93adaa1e4ca19aa040c2982d5ef958d70fe97
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-09 18:32:56 +0530

    Update tagalerts.py

--- Zaid/modules/tagalerts.py
+++ Zaid/modules/tagalerts.py
@@ -5,7 +5,7 @@ Lovely will notify it to you in private message after enabling tag alerts
 How to use this feature ?
 ❂ `/tagalert on` : Turn tag alerts on
 ❂ `/tagalert off` : Turn tag alerts off
-
+"""
 __mod_name__ = "Tag alert"
 
 __lovely_advance__ = __help__
