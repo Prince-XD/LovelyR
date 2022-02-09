fc58a52252112a45d9ac16ce7e86883262e0c6ed
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-09 14:07:49 +0530

    Delete mute.py

--- Zaid/modules/mute.py
+++
@@ -1,9 +0,0 @@
-__help__ = """
-**Admin commands**
-❂ /mute <userhandle>*:* silences a user. Can also be used as a reply, muting the replied to user.
-❂ /tmute <userhandle> x(m/h/d)*:* mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
-❂ /unmute <userhandle>*:* unmutes a user. Can also be used as a reply, muting the replied to user.
-"""
-__mod_name = "Mute"
-
-__lovely_basic__ = __help__
