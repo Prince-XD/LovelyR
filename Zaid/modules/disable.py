ecf66d2675f7a090677018c254147c6f75e90f7a
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-09 14:17:34 +0530

    Update disable.py

--- Zaid/modules/disable.py
+++ Zaid/modules/disable.py
@@ -316,21 +316,6 @@ if is_module_loaded(FILENAME):
     def __chat_settings__(chat_id, user_id):
         return build_curr_disabled(chat_id)
 
-
-    __help__ = """
-❂ /cmds*:* check the current status of disabled commands
-
-*Admins only:*
-
-❂ /enable <cmd name>*:* enable that command
-❂ /disable <cmd name>*:* disable that command
-❂ /enablemodule <module name>*:* enable all commands in that module
-❂ /disablemodule <module name>*:* disable all commands in that module
-❂ /listcmds*:* list all possible toggleable commands
-"""
-
-__lovely_basic__ = __help__
-
     DISABLE_HANDLER = CommandHandler("disable", disable, run_async=True)
     DISABLE_MODULE_HANDLER = CommandHandler(
         "disablemodule", disable_module, run_async=True
@@ -349,7 +334,6 @@ __lovely_basic__ = __help__
     dispatcher.add_handler(COMMANDS_HANDLER)
     dispatcher.add_handler(TOGGLE_HANDLER)
 
-    __mod_name__ = "Disabling"
 
 else:
     DisableAbleCommandHandler = CommandHandler
