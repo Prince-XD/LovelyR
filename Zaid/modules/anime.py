400c81a4dbac5b4b3de364b8e0298a561edc8f46
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-10 07:20:21 +0530

    Update anime.py

--- Zaid/modules/anime.py
+++ Zaid/modules/anime.py
@@ -589,6 +589,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
+__lovely_tools__ = __help__
 __mod_name__ = "Anime"
 __command_list__ = [
     "anime", "manga", "character", "user", "upcoming", "kaizoku", "airing",
