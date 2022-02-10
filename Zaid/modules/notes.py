24e363dcc99b7a84d52b80227ff2b7dfd3198eeb
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-02-10 08:12:22 +0530

    Update notes.py

--- Zaid/modules/notes.py
+++ Zaid/modules/notes.py
@@ -575,6 +575,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 """
 
 __mod_name__ = "Notes"
+__lovely_basic = __help__
 
 GET_HANDLER = CommandHandler("get", cmd_get, run_async=True)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get, run_async=True)
